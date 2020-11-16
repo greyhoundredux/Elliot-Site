e39c20d03063c3cabfc378ced4bea831070edb67
Elliot <elliot.danby@hotmail.com>
2020-11-16 17:34:40 +0000

    added skills functionality, some polishing of aesthetics

--- script.js
+++ script.js
@@ -13,34 +13,46 @@ function showDescription(obj) {
 }
 
 function boxClose(obj) {
- if (obj == document.getElementById('html')) {
-     document.getElementById('javascript').style.display = "none";
-     document.getElementById('java').style.display = "none";
-     document.getElementById('python').style.display = "none";
-     document.getElementById('c').style.display = "none";
- }
- if (obj == document.getElementById('javascript')) {
-     document.getElementById('html').style.display = "none";
-     document.getElementById('java').style.display = "none";
-     document.getElementById('python').style.display = "none";
-     document.getElementById('c').style.display = "none";
- }
- if (obj == document.getElementById('java')) {
-     document.getElementById('html').style.display = "none";
-     document.getElementById('javascript').style.display = "none";
-     document.getElementById('python').style.display = "none";
-     document.getElementById('c').style.display = "none";
- }
- if (obj == document.getElementById('python')) {
-     document.getElementById('html').style.display = "none";
-     document.getElementById('javascript').style.display = "none";
-     document.getElementById('java').style.display = "none";
-     document.getElementById('c').style.display = "none";
- }
- if (obj == document.getElementById('c')) {
-     document.getElementById('html').style.display = "none";
-     document.getElementById('javascript').style.display = "none";
-     document.getElementById('java').style.display = "none";
-     document.getElementById('python').style.display = "none";
- }
+    if (obj == document.getElementById('html')) {
+        document.getElementById('javascript').style.display = "none";
+        document.getElementById('java').style.display = "none";
+        document.getElementById('python').style.display = "none";
+        document.getElementById('c').style.display = "none";
+    }
+    if (obj == document.getElementById('javascript')) {
+        document.getElementById('html').style.display = "none";
+        document.getElementById('java').style.display = "none";
+        document.getElementById('python').style.display = "none";
+        document.getElementById('c').style.display = "none";
+    }
+    if (obj == document.getElementById('java')) {
+        document.getElementById('html').style.display = "none";
+        document.getElementById('javascript').style.display = "none";
+        document.getElementById('python').style.display = "none";
+        document.getElementById('c').style.display = "none";
+    }
+    if (obj == document.getElementById('python')) {
+        document.getElementById('html').style.display = "none";
+        document.getElementById('javascript').style.display = "none";
+        document.getElementById('java').style.display = "none";
+        document.getElementById('c').style.display = "none";
+    }
+    if (obj == document.getElementById('c')) {
+        document.getElementById('html').style.display = "none";
+        document.getElementById('javascript').style.display = "none";
+        document.getElementById('java').style.display = "none";
+        document.getElementById('python').style.display = "none";
+    }
+    if (obj ==document.getElementById('mysql')) {
+        document.getElementById('git').style.display = "none";
+        document.getElementById('adobe').style.display = "none";
+    }
+    if (obj ==document.getElementById('git')) {
+        document.getElementById('mysql').style.display = "none";
+        document.getElementById('adobe').style.display = "none";
+    }
+    if (obj ==document.getElementById('adobe')) {
+        document.getElementById('mysql').style.display = "none";
+        document.getElementById('git').style.display = "none";
+    }
 }
