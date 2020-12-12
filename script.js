4e1d37115c372996af80b45ce1b6fcf3c4e03503
Elliot <elliot.danby@hotmail.com>
2020-12-12 12:40:48 +0000

    fixed some janky-ness on mobile view

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function themeSwitcher() {
 function showDescription(obj) {
     var obj=document.getElementById(obj);
     if (obj.style.display == "block")
-    obj.style.display = "none";
+        obj.style.display = "none";
     else {
         obj.style.display = "block";
         boxClose(obj);
@@ -43,16 +43,71 @@ function boxClose(obj) {
         document.getElementById('java').style.display = "none";
         document.getElementById('python').style.display = "none";
     }
-    if (obj ==document.getElementById('mysql')) {
+    if (obj == document.getElementById('mysql')) {
         document.getElementById('git').style.display = "none";
         document.getElementById('adobe').style.display = "none";
     }
-    if (obj ==document.getElementById('git')) {
+    if (obj == document.getElementById('git')) {
         document.getElementById('mysql').style.display = "none";
         document.getElementById('adobe').style.display = "none";
     }
-    if (obj ==document.getElementById('adobe')) {
+    if (obj == document.getElementById('adobe')) {
         document.getElementById('mysql').style.display = "none";
         document.getElementById('git').style.display = "none";
+    }
+}
+
+function hideCard(obj) {
+    var obj=document.getElementById(obj);
+    if (obj.style.display !== "none") {
+        obj.style.display = "none";
+        changeButton(obj);
+    }
+    else {
+        obj.style.display = "block";
+        changeButton(obj);
+    }
+}
+
+function changeButton(obj) {
+    if (obj == document.getElementById('lang-hide')) {
+        if (document.getElementById('lang-hide-button').style.display !== "none") {
+            document.getElementById('lang-hide-button').style.display = "none";
+            document.getElementById('lang-show-button').style.display = "block";
+        }
+        else {
+            document.getElementById('lang-hide-button').style.display = "block";
+            document.getElementById('lang-show-button').style.display = "none";
+        }
+    }
+    if (obj == document.getElementById('skills-hide')) {
+        if (document.getElementById('skills-hide-button').style.display !== "none") {
+            document.getElementById('skills-hide-button').style.display = "none";
+            document.getElementById('skills-show-button').style.display = "block";
+        }
+        else {
+            document.getElementById('skills-hide-button').style.display = "block";
+            document.getElementById('skills-show-button').style.display = "none";
+        }
+    }
+    if (obj == document.getElementById('education-hide')) {
+        if (document.getElementById('edu-hide-button').style.display !== "none") {
+            document.getElementById('edu-hide-button').style.display = "none";
+            document.getElementById('edu-show-button').style.display = "block";
+        }
+        else {
+            document.getElementById('edu-hide-button').style.display = "block";
+            document.getElementById('edu-show-button').style.display = "none";
+        }
+    }
+    if (obj == document.getElementById('employment-hide')) {
+        if (document.getElementById('employment-hide-button').style.display !== "none") {
+            document.getElementById('employment-hide-button').style.display = "none";
+            document.getElementById('employment-show-button').style.display = "block";
+        }
+        else {
+            document.getElementById('employment-hide-button').style.display = "block";
+            document.getElementById('employment-show-button').style.display = "none";
+        }
     }
 }
